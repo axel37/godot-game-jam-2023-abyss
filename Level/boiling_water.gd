62b8e8db55e1bf1b2a6dfffd13dc2da8fb488e2b
Axel Choteau <axelc37@outlook.fr>
2023-04-01 12:48:40 +0200

    Hot zone is now area2d (instead of staticbody)

--- Level/boiling_water.gd
+++ Level/boiling_water.gd
@@ -1,6 +1,13 @@
-extends StaticBody2D
+extends Area2D
 
 @export var speed: float = 10
 
+
 func _physics_process(delta):
 	position.y += speed * delta
+	
+	var colliding_bodies = get_overlapping_bodies()
+	
+	for body in colliding_bodies:
+		if body is CharacterBody2D:
+			body.position.y += speed * delta
