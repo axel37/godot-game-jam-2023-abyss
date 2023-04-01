40c15c56c01c4edb8ccb1bd32c077393ec00e6ff
Axel Choteau <axelc37@outlook.fr>
2023-04-01 09:59:23 +0200

    Dash particles, refactored dash code into functions

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
+# Player config
 @export var dash_speed = 200
 @export var rotation_speed = 0.1
 
+# Instance variables
+@onready var dash_particles: GPUParticles2D = %DashParticles
 var is_dashing: bool = false
 var current_dash_speed: float = 0
 
@@ -13,17 +16,28 @@ func _physics_process(delta):
 		rotate(rotation_speed)
 
 	if Input.is_action_just_pressed("dash") and not is_dashing:
-		current_dash_speed = dash_speed
-		is_dashing = true
+		start_dash()
 	
 	if is_dashing:
-		velocity = transform.x * current_dash_speed
-		current_dash_speed -= dash_speed / 50
-		if current_dash_speed <= 0:
-			is_dashing = false
-			current_dash_speed = 0
+		process_dash()
 
 	
 	move_and_slide()
 	
 	velocity *= 0.9
+	
+func start_dash():
+	is_dashing = true
+	current_dash_speed = dash_speed
+	dash_particles.emitting = true
+
+func stop_dash():
+	is_dashing = false
+	current_dash_speed = 0
+	dash_particles.emitting = false
+
+func process_dash():
+	velocity = transform.x * current_dash_speed
+	current_dash_speed -= dash_speed / 50
+	if current_dash_speed <= 0:
+		stop_dash()
