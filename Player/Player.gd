68532fd88c100af780988ac999775faec1de678a
Axel Choteau <axelc37@outlook.fr>
2023-04-01 12:07:53 +0200

    Stun player on collision

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 # Player config
 @export var dash_speed = 200
 @export var rotation_speed = 0.1
+@export var stun_duration_multiplier = 0.5
 
 # Instance variables
 @onready var dash_particles: GPUParticles2D = %DashParticles
@@ -11,15 +12,23 @@ var collision_particles: PackedScene = preload("res://Particles/collision_partic
 
 var is_dashing: bool = false
 var current_dash_speed: float = 0
+var is_stunned: bool = false
+var remaining_stun_duration: float = 0
 
 func _physics_process(delta):
-	if Input.is_action_pressed("rotate_left"):
-		rotate(-rotation_speed)
-	if Input.is_action_pressed("rotate_right"):
-		rotate(rotation_speed)
+	if is_stunned:
+		remaining_stun_duration -= delta
+		if remaining_stun_duration <= 0:
+			is_stunned = false
 	
-	if Input.is_action_just_pressed("dash") and not is_dashing:
-		start_dash()
+	if not is_stunned:
+		if Input.is_action_pressed("rotate_left"):
+			rotate(-rotation_speed)
+		if Input.is_action_pressed("rotate_right"):
+			rotate(rotation_speed)
+		
+		if Input.is_action_just_pressed("dash") and not is_dashing:
+			start_dash()
 	
 	if is_dashing:
 		process_dash()
@@ -30,13 +39,26 @@ func _physics_process(delta):
 
 func process_movement(delta: float):
 	var collision: KinematicCollision2D = move_and_collide(velocity * delta)
+	# If a collision occurs, bounce and stun the player
 	if collision:
-		var particle_instance = collision_particles.instantiate()
-		add_child(particle_instance)
-		particle_instance.emitting = true
+		# The higher the velocity, the longer the stun
+		stun(collision.get_remainder().length())
+		# Spawn particles
+		spawn_collision_particles()
+		# Force stop dash
 		stop_dash()
+		# Bounce the player
 		velocity = velocity.bounce(collision.get_normal()) * 2
 
+# Stun the player for the specified duration
+func stun(duration):
+	is_stunned = true
+	remaining_stun_duration = duration * stun_duration_multiplier
+
+func spawn_collision_particles():
+	var particle_instance = collision_particles.instantiate()
+	add_child(particle_instance)
+	particle_instance.emitting = true
 
 func start_dash():
 	is_dashing = true
